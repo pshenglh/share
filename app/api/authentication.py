0ac20d2b85c0d18f233fed44b0066806780fbea9
pshenglh <psheng@outlook.com>
2017-06-14 11:31:50 +0800

    令牌认证

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -7,14 +7,19 @@ from . import api
 auth = HTTPBasicAuth()
 
 @auth.verify_password
-def verify_password(email, password):
-    if email == '':
+def verify_password(email_or_token, password):
+    if email_or_token == '':
         g.current_user = AnonymousUser
         return True
-    user = Users.objects(email=email).first()
+    if password == '':
+        g.current_user = Users.verify_auth_token(email_or_token)
+        g.token_used = True
+        return g.current_user is not None
+    user = Users.objects(email=email_or_token).first()
     if not user:
         return False
     g.current_user = user
+    g.token_used = False
     return user.verify_password(password)
 
 @auth.error_handler
@@ -23,6 +28,9 @@ def auth_error():
 
 
 @api.route('/token')
-def git_token():
+def get_token():
     if g.current_user.is_anonymous or g.token_used:
         return unauthorized('Invalid credentials')
+    return jsonify({'token': g.current_user.generate_auth_token(
+        expiration=3600), 'expiration': 3600
+    })
--- app/models.py
+++ app/models.py
@@ -45,6 +45,21 @@ class Users(db.Document, UserMixin):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'confirm': self.user_id})
 
+
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'],
+                       expires_in=expiration)
+        return s.dumps({'id': self.user_id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return Users.objects(user_id=data['id']).first()
+
     def confirm(self, token):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
